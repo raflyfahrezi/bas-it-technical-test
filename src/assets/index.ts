7bb2e1be105357d3746de08f139229ed5c6d9c57
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-20 10:31:16 +0700

    adding assets

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import HomeIcon from './home'
+import SalesIcon from './sales'
+import CompanyIcon from './company'
+import FinanceIcon from './finance'
+import CompetitorIcon from './competitor'
+import QuestionsIcon from './questions'
+
+export {
+  HomeIcon,
+  SalesIcon,
+  CompanyIcon,
+  FinanceIcon,
+  CompetitorIcon,
+  QuestionsIcon,
+}
